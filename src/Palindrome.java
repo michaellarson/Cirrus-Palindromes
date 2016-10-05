ec6b2be590664d00b5f4d44e2b1fa52d3ab7b662
Michael Larson <michael.larson21@gmail.com>
2016-10-05 15:06:02 -0400

    Used JavaFX to create UI

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,5 +1,19 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
 
-public class Palindrome {
+public class Palindrome extends Application {
 	
 	//Determines if string length is even or odd
 	public static boolean isEven(String word){
@@ -16,29 +30,30 @@ public class Palindrome {
 	}
 	
 	//Finds highest palindrome number between two given numbers
-	public static String findPalindrome(int highNum, int lowNum){
-		String snum = Integer.toString(highNum);
-		String frontHalf = getFrontHalf(snum);
+	public static String findPalindrome(String highNum, String lowNum){
+		highNum = highNum.replace(",", "");
+		lowNum = lowNum.replace(",", "");
+		String frontHalf = getFrontHalf(highNum);
 		String palindrome = "";
-		if(isEven(snum)){
+		if(isEven(highNum)){
 			palindrome = frontHalf + new StringBuilder(frontHalf).reverse().toString();
-			if(Integer.parseInt(palindrome) <= lowNum)
+			if(Integer.parseInt(palindrome) <= Integer.parseInt(lowNum))
 				return "No palindrome found.";
-			else if(Integer.parseInt(palindrome) >= highNum){
+			else if(Integer.parseInt(palindrome) >= Integer.parseInt(highNum)){
 				frontHalf = Integer.toString(Integer.parseInt(frontHalf) - 1);
 				palindrome = frontHalf + new StringBuilder(frontHalf).reverse().toString();
-				if(Integer.parseInt(palindrome) <= lowNum)
+				if(Integer.parseInt(palindrome) <= Integer.parseInt(lowNum))
 					return "No palindrome found.";
 			}
 		}
 		else{
 			palindrome = frontHalf + new StringBuilder(frontHalf.substring(0, frontHalf.length()-1)).reverse().toString();
-			if(Integer.parseInt(palindrome) <= lowNum)
+			if(Integer.parseInt(palindrome) <= Integer.parseInt(lowNum))
 				return "No palindrome found.";
-			else if(Integer.parseInt(palindrome) >= highNum){
+			else if(Integer.parseInt(palindrome) >= Integer.parseInt(highNum)){
 				frontHalf = Integer.toString(Integer.parseInt(frontHalf) - 1);
 				palindrome = frontHalf + new StringBuilder(frontHalf.substring(0, frontHalf.length()-1)).reverse().toString();
-				if(Integer.parseInt(palindrome) <= lowNum)
+				if(Integer.parseInt(palindrome) <= Integer.parseInt(lowNum))
 					return "No palindrome found.";
 			}
 		}
@@ -47,8 +62,46 @@ public class Palindrome {
 	}
 	
 	public static void main(String[] args){
-		int a = 46332;
-		int b = 46260;
-		System.out.println(findPalindrome(a,b));
-	}
+        launch(args);
+    }
+	
+	//UI Creation
+	public void start(Stage primaryStage){
+        primaryStage.setTitle("Palindrome Finder");
+        GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+		
+		Label highLabel = new Label("High Number");
+		grid.add(highLabel, 0, 1);
+        TextField highNum = new TextField();
+        highNum.setTextFormatter(new TextFormatter<>(new NumberStringConverter()));
+        grid.add(highNum, 1, 1);
+        Label lowLabel = new Label("Low Number");
+		grid.add(lowLabel, 0, 2);
+        TextField lowNum = new TextField();
+        lowNum.setTextFormatter(new TextFormatter<>(new NumberStringConverter()));
+        grid.add(lowNum, 1, 2);
+        
+        Button search = new Button("Find Palindrome");
+        search.setMinWidth(100);
+        grid.add(search, 0, 3, 2, 1);
+        
+        Label outLabel = new Label("Palindrome:");
+        grid.add(outLabel, 0, 4);
+        Text output = new Text();
+        grid.add(output, 1, 4);
+        
+        search.setOnAction(new EventHandler<ActionEvent>(){
+            public void handle(ActionEvent event) {
+                output.setText(findPalindrome(highNum.getText(),lowNum.getText()));
+            }
+        });
+        
+        Scene scene = new Scene(grid, 300, 225);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
